1bfeaa9718b3b99bfc0c9e716f5f275bdb690a06
Eduardo Bordin <99068107+EduardoSBordin@users.noreply.github.com>
2023-02-14 00:56:05 -0300

    v1.1

--- script.js
+++ script.js
@@ -4,20 +4,41 @@
 
 const num = document.querySelector('#num');
 const btnResult = document.querySelector('#btnResult');
+const btnRemove = document.querySelector('#btnRemove');
 const resultTxt = document.querySelector('#resultTxt');
 
-
-function factorialCalculation(){
-    
-}
 btnResult.addEventListener('click', () => {
+
     
-    let factorial = num.value;
-    // let result = factorial;
-    let getFirstNum = factorial - 1;
-    for(var i = getFirstNum; i > 1; i--){
-     factorial *= i;
+    let result = num.value;
+    // let getFirstNum = result - 1;
+    // for(var i = getFirstNum; i >= 1; i--){
+    //     result *= i;
+    // }
+    // console.log('2-' + result);
+    //-------------------------------------
+    // localStorage.setItem('num', result);
+    // resultTxt.innerHTML = localStorage.getItem('num');
+    // resultTxt.innerHTML = rf;
+
+    for(let i = result; i >= 1; i--){
+
+        let getResult = (result - 1) * result;
+        
+        console.log('For: ' + getResult); 
+
     }
-    console.log(factorial);
-    resultTxt.innerHTML = factorial;
+
+    // if(result >= 1){
+    //     result = (result - 1) * result;
+    //     console.log('IF: ' + result); 
+
+    // }
+});
+resultTxt.innerHTML = localStorage.getItem('num');
+
+btnRemove.addEventListener('click', () => {
+
+    localStorage.removeItem('num');
+    resultTxt.innerHTML = '0';
 });
