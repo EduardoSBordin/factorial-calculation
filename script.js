639f12765032c87d830190763f00f91066601528
Eduardo Bordin <99068107+EduardoSBordin@users.noreply.github.com>
2023-02-14 02:57:27 -0300

    v2.1

--- script.js
+++ script.js
@@ -5,12 +5,13 @@
 const num = document.querySelector('#num');
 const btnResult = document.querySelector('#btnResult');
 const btnRemove = document.querySelector('#btnRemove');
+const resultTxt0 = document.querySelector('#resultTxt0');
 const resultTxt1 = document.querySelector('#resultTxt1');
 const resultTxt2 = document.querySelector('#resultTxt2');
 const resultTxt3 = document.querySelector('#resultTxt3');
 
-btnResult.addEventListener('click', () => {
-    
+function calc(){
+
     let result = num.value;
     let getFirstNum = result - 1;
 
@@ -18,26 +19,32 @@ btnResult.addEventListener('click', () => {
 
     for(var i = getFirstNum; i > 1; i--){
 
+        resultTxt0.innerHTML = `N! = ${num.value}`;
+        resultTxt1.innerHTML += `x ${getFirstNum} `;
         result *= i;
-        console.log('Teste: ' + i);
         
         let getI = i - 1;
         console.log(getI + 'x' + result + ' : ' + getI * result);
         console.log(`O resultado de: ${i+1} x ${getFirstNum} : ${result}`);
 
         getFirstNum--;
-        resultTxt1.innerHTML = `<br>Expressao: ${i}<br>`;
-        resultTxt3.innerHTML += `<br> O resultado de: ${getFirstNum} x ${result} : ${getI * result} <br>`;
+        resultTxt3.innerHTML += `<br>Resultado de: ${getFirstNum} x ${result} : ${getI * result} <br>`;
         
     }
-    resultTxt2.innerHTML = `<br> O primeiro resultado de: ${num.value} x ${num.value - 1} : ${getRes} <br>`;
+    
+   let getRes2 = resultTxt2.innerHTML = `<br><br>Resultado de: ${num.value} x ${num.value - 1} : ${getRes} <br>`;
     // //-------------------------------------
-    // // localStorage.setItem('num', result);
-    // // resultTxt.innerHTML = localStorage.getItem('num');
+    localStorage.setItem('res2', getRes2);
+    let loGet = localStorage.getItem('res2');
+    resultTxt2.innerHTML = loGet;
+}
 
+btnResult.addEventListener('click', () => {
+    
+calc();
    
 });
-resultTxt1.innerHTML = localStorage.getItem('num');
+
 
 btnRemove.addEventListener('click', () => {
 
