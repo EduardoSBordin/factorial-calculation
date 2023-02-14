f96bbf4e90cd7aa34a846f007eae90cbfa87ab22
Eduardo Bordin <99068107+EduardoSBordin@users.noreply.github.com>
2023-02-14 02:34:19 -0300

    v2.0

--- script.js
+++ script.js
@@ -5,40 +5,42 @@
 const num = document.querySelector('#num');
 const btnResult = document.querySelector('#btnResult');
 const btnRemove = document.querySelector('#btnRemove');
-const resultTxt = document.querySelector('#resultTxt');
+const resultTxt1 = document.querySelector('#resultTxt1');
+const resultTxt2 = document.querySelector('#resultTxt2');
+const resultTxt3 = document.querySelector('#resultTxt3');
 
 btnResult.addEventListener('click', () => {
-
     
     let result = num.value;
-    // let getFirstNum = result - 1;
-    // for(var i = getFirstNum; i >= 1; i--){
-    //     result *= i;
-    // }
-    // console.log('2-' + result);
-    //-------------------------------------
-    // localStorage.setItem('num', result);
-    // resultTxt.innerHTML = localStorage.getItem('num');
-    // resultTxt.innerHTML = rf;
+    let getFirstNum = result - 1;
 
-    for(let i = result; i >= 1; i--){
+    let getRes = result * getFirstNum;
 
-        let getResult = (result - 1) * result;
+    for(var i = getFirstNum; i > 1; i--){
+
+        result *= i;
+        console.log('Teste: ' + i);
         
-        console.log('For: ' + getResult); 
+        let getI = i - 1;
+        console.log(getI + 'x' + result + ' : ' + getI * result);
+        console.log(`O resultado de: ${i+1} x ${getFirstNum} : ${result}`);
 
+        getFirstNum--;
+        resultTxt1.innerHTML = `<br>Expressao: ${i}<br>`;
+        resultTxt3.innerHTML += `<br> O resultado de: ${getFirstNum} x ${result} : ${getI * result} <br>`;
+        
     }
+    resultTxt2.innerHTML = `<br> O primeiro resultado de: ${num.value} x ${num.value - 1} : ${getRes} <br>`;
+    // //-------------------------------------
+    // // localStorage.setItem('num', result);
+    // // resultTxt.innerHTML = localStorage.getItem('num');
 
-    // if(result >= 1){
-    //     result = (result - 1) * result;
-    //     console.log('IF: ' + result); 
-
-    // }
+   
 });
-resultTxt.innerHTML = localStorage.getItem('num');
+resultTxt1.innerHTML = localStorage.getItem('num');
 
 btnRemove.addEventListener('click', () => {
 
     localStorage.removeItem('num');
-    resultTxt.innerHTML = '0';
+    resultTxt1.innerHTML = '0';
 });
